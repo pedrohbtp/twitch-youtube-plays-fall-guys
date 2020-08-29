cd0caef4e52add1b7b09233d7da5295f14073a84
pedro_perez <pedrohbtp@gmail.com>
2020-08-28 21:07:13 -0300

    feat: changed key codes and refactored style

--- key.py
+++ key.py
@@ -10,25 +10,26 @@ import time, sys
 keyDelay = 0.1
 # https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 keymap = {
-    "Up": win32con.VK_UP,
-    "Left": win32con.VK_LEFT,
-    "Down": win32con.VK_DOWN,
-    "Right": win32con.VK_RIGHT,
-    "b": 0x42, # ord("B"),
-    "a": 0x41, # ord("A"),
-    "y": 0x59, # ord("Y"),  # for DS
-    "x": 0x58, # ord("X"),  # for DS
-    "s": 0x53, # ord("S"),  # Start
-    "e": 0x45, # ord("E"),  # Select
+    "up": win32con.VK_UP,
+    "down": win32con.VK_DOWN,
+    "left": win32con.VK_LEFT,
+    "right": win32con.VK_RIGHT,
+    "grab": win32con.VK_LSHIFT,
+    "jump": win32con.VK_SPACE,
+    "dive": win32con.VK_LCONTROL
 }
 
 # this way has to keep window in focus
 def sendKey(button):
+    ''' Sends the button press to the screen in focus
+    '''
     win32api.keybd_event(keymap[button], 0, 0, 0)
     time.sleep(keyDelay)
     win32api.keybd_event(keymap[button], 0, win32con.KEYEVENTF_KEYUP, 0)
 
 def SimpleWindowCheck(windowName):
+    ''' Tries to find window by name
+    '''
     window = None
     try:
         window = win32gui.FindWindow(windowName, None)
@@ -42,6 +43,15 @@ def SimpleWindowCheck(windowName):
     else:
         return window
 
+def enumHandler(hwnd, extra):
+    ''' Function used to append to a list all 
+    the windows that have the configured windowName present
+    hwnd: handlwer passed automatically by win32gui.EnumWindows
+    extra: dictionary like { 'return_list': list, 'window_name': str}
+    '''
+    if extra['window_name'].lower() in win32gui.GetWindowText(hwnd).lower():
+        extra['return_list'].append({'screen_code': hwnd, 'screen_title': win32gui.GetWindowText(hwnd)})
+
 if __name__ == "__main__":
     windowName = sys.argv[1]
     key = sys.argv[2]
@@ -51,18 +61,14 @@ if __name__ == "__main__":
 
     if not (winId):
         windowList = []
-        
-        def enumHandler(hwnd, list):
-            if windowName in win32gui.GetWindowText(hwnd):
-                list.append(hwnd)
-        
-        win32gui.EnumWindows(enumHandler, windowList)
+        # gets the windows that contain the string in windowName
+        win32gui.EnumWindows(enumHandler, { 'return_list': windowList, 'window_name': windowName})
         # only the first id, may need to try the others
-        winId = windowList[0]
+        winId = windowList[0]['screen_code']
 
         # can check with this
-        for hwnd in windowList:
-            hwndChild = win32gui.GetWindow(hwnd, win32con.GW_CHILD)
+        for window in windowList:
+            hwndChild = win32gui.GetWindow(window['screen_code'], win32con.GW_CHILD)
             # print("window title/id/child id: ", win32gui.GetWindowText(hwnd), "/", hwnd, "/", hwndChild)
 
     win32gui.ShowWindow(winId, win32con.SW_SHOWNORMAL)
