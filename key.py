e55d2250c08c921e6fb146281fffe00453034487
pedro_perez <pedrohbtp@gmail.com>
2020-08-30 14:32:18 -0300

    refactor: refactored key file to be used by python script

--- key.py
+++ key.py
@@ -10,13 +10,13 @@ import time, sys
 keyDelay =0.1
 # https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 keymap = {
-    "up": 0x57, #w
-    "down": 0x53, #s
-    "left": 0x41, #a
-    "right": 0x44, #d
-    "grab": win32con.VK_LSHIFT,
-    "jump": win32con.VK_SPACE,
-    "dive": win32con.VK_LCONTROL,
+    "w": 0x57, #w
+    "s": 0x53, #s
+    "a": 0x41, #a
+    "d": 0x44, #d
+    "l_shift": win32con.VK_LSHIFT,
+    "space": win32con.VK_SPACE,
+    "l_control": win32con.VK_LCONTROL,
     "esc": win32con.VK_ESCAPE
 }
 
@@ -36,15 +36,15 @@ def send_key(button):
     time.sleep(keyDelay)
     win32api.keybd_event(keymap[button], 0, win32con.KEYEVENTF_KEYUP, 0)
 
-def SimpleWindowCheck(windowName):
+def SimpleWindowCheck(window_name):
     ''' Tries to find window by name
     '''
     window = None
     try:
-        window = win32gui.FindWindow(windowName, None)
+        window = win32gui.FindWindow(window_name, None)
     except win32gui.error:
         try: 
-            window = win32gui.FindWindow(None, windowName)
+            window = win32gui.FindWindow(None, window_name)
         except win32gui.error:
             return False
         else:
@@ -54,7 +54,7 @@ def SimpleWindowCheck(windowName):
 
 def enumHandler(hwnd, extra):
     ''' Function used to append to a list all 
-    the windows that have the configured windowName present
+    the windows that have the configured window_name present
     hwnd: handlwer passed automatically by win32gui.EnumWindows
     extra: dictionary like { 'return_list': list, 'window_name': str}
     '''
@@ -62,35 +62,16 @@ def enumHandler(hwnd, extra):
         extra['return_list'].append({'screen_code': hwnd, 'screen_title': win32gui.GetWindowText(hwnd)})
 
 
-if __name__ == "__main__":
+def send_key_to_window(window_name, key, hold_time_seconds):
+    start_exec_time_s = time.time()
 
-    # tests for input validity
-    if len(sys.argv) < 3:
-        print('Invalid number of arguments!')
-        print('Syntax: python key.py <window_title> <action> <optional_hold_time_in_secons>')
-        exit(0)
-
-    windowName = sys.argv[1]
-    key = sys.argv[2]
-    try:
-        hold_time_seconds = float(sys.argv[3]) if len(sys.argv) >=4 else None
-    except ValueError as e:
-        # put = -1 to exit on the next if
-        hold_time_seconds = -1
-
-
-    # limits the hold time to 5 seconds
-    if hold_time_seconds!= None and(  hold_time_seconds > 5 or hold_time_seconds < 1):
-        print('hold time must be within 1 and 5')
-        exit(0)
-
-    winId = SimpleWindowCheck(windowName)
+    winId = SimpleWindowCheck(window_name)
     # winId = None
 
     if not (winId):
         windowList = []
-        # gets the windows that contain the string in windowName
-        win32gui.EnumWindows(enumHandler, { 'return_list': windowList, 'window_name': windowName})
+        # gets the windows that contain the string in window_name
+        win32gui.EnumWindows(enumHandler, { 'return_list': windowList, 'window_name': window_name})
         # only the first id, may need to try the others
         winId = windowList[0]['screen_code']
 
@@ -101,7 +82,36 @@ if __name__ == "__main__":
 
     win32gui.ShowWindow(winId, win32con.SW_SHOWNORMAL)
     win32gui.SetForegroundWindow(winId)
+    print('Sending command using python. Key: ', key)
     if hold_time_seconds == None:
         send_key(key)
     else:
         press_for_seconds(key, hold_time_seconds)
+    
+    print('Finished sending python command. Key: ', key)
+    print('Python time in s: ',time.time() - start_exec_time_s)
+
+
+if __name__ == "__main__":
+    # tests for input validity
+    if len(sys.argv) < 3:
+        print('Invalid number of arguments!')
+        print('Syntax: python key.py <window_title> <action> <optional_hold_time_in_secons>')
+        exit(0)
+
+    window_name = sys.argv[1]
+    key = sys.argv[2]
+    try:
+        hold_time_seconds = float(sys.argv[3]) if len(sys.argv) >=4 else None
+    except ValueError as e:
+        # put = -1 to exit on the next if
+        hold_time_seconds = -1
+    
+    
+    # limits the hold time to 5 seconds
+    if hold_time_seconds!= None and(  hold_time_seconds > 5 or hold_time_seconds < 1):
+        print('hold time must be within 1 and 5')
+        exit(0)
+
+    send_key_to_window(window_name, key, hold_time_seconds)
+    
